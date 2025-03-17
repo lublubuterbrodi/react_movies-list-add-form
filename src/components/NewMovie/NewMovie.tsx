8d72f8dd6c317a1fa6042937160388cbb8fd617d
lublubuterbrodi <lublubuterbrodi@gmail.com>
2025-03-17 12:00:52 +0200

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,118 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+const urlPattern =
+  // eslint-disable-next-line max-len
+  /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+interface Props {
+  onAdd: (movie: Movie) => void;
+}
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [movie, setMovie] = useState<Movie>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const [touched, setTouched] = useState<Record<string, boolean>>({});
+
+  const handleChange = (name: keyof Movie, value: string) => {
+    setMovie(prev => ({ ...prev, [name]: value }));
+  };
+
+  const isValid = (): boolean => {
+    return (
+      movie.title.trim() !== '' &&
+      movie.imgUrl.trim() !== '' &&
+      movie.imdbUrl.trim() !== '' &&
+      movie.imdbId.trim() !== '' &&
+      urlPattern.test(movie.imgUrl) &&
+      urlPattern.test(movie.imdbUrl)
+    );
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!isValid()) {
+      return;
+    }
+
+    onAdd({ ...movie });
+
+    setMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+    setTouched({});
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movie.title}
+        onChange={value => handleChange('title', value)}
         required
+        data-cy="movie-title"
+      />
+      {touched.title && !movie.title.trim() && (
+        <p className="help is-danger">Title is required</p>
+      )}
+
+      <TextField
+        name="description"
+        label="Description"
+        value={movie.description}
+        onChange={value => handleChange('description', value)}
+        data-cy="movie-description"
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={movie.imgUrl}
+        onChange={value => handleChange('imgUrl', value)}
+        required
+        data-cy="movie-imgUrl"
+      />
+      {touched.imgUrl && !urlPattern.test(movie.imgUrl) && (
+        <p className="help is-danger">Invalid URL</p>
+      )}
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={movie.imdbUrl}
+        onChange={value => handleChange('imdbUrl', value)}
+        required
+        data-cy="movie-imdbUrl"
+      />
+      {touched.imdbUrl && !urlPattern.test(movie.imdbUrl) && (
+        <p className="help is-danger">Invalid URL</p>
+      )}
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
-
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={movie.imdbId}
+        onChange={value => handleChange('imdbId', value)}
+        required
+        data-cy="movie-imdbId"
+      />
+      {touched.imdbId && !movie.imdbId.trim() && (
+        <p className="help is-danger">IMDB ID is required</p>
+      )}
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +120,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isValid()}
           >
             Add
           </button>
